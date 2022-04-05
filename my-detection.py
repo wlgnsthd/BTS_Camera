a63b9396cab1e07f786b4de38b41f73b54bad0e0
Joon Song <thdwlgnsdlek@mail.ulsan.ac.kr>
2022-04-05 20:05:50 +0900

    Update my-detection.py

--- my-detection.py
+++ my-detection.py
@@ -2,11 +2,34 @@ import jetson.inference
 import jetson.utils
 
 net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
-camera = jetson.utils.videoSource("csi://1")      # '/dev/video0' for V4L2 # csi://0
-display = jetson.utils.videoOutput() # 'my_video.mp4' for file # "display://0"
-#display.IsStreaming()
-while True: 
-	img = camera.Capture()
-	detections = net.Detect(img)
-	display.Render(img)
-	display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
+camera = jetson.utils.videoSource("/dev/video0")      # '/dev/video0' or 'csi://0'
+display = jetson.utils.videoOutput("display://0")     # 'my_video.mp4' for file
+
+
+object_count = 0
+
+while display.IsStreaming():
+    img = camera.Capture()
+    detections = net.Detect(img)#overlay='none'
+
+    for detection in detections:
+        # allocate the output image, with the cropped size
+        imgCropped = jetson.utils.cudaAllocMapped(width=detection.Width,
+                                                  height=detection.Height,
+                                                  format=img.format)
+
+        # get the cropping ROI from the bounding box
+        #crop_roi = (detection.Left, detection.Top, detection.Right, detection.Bottom)
+        print(detection.Left, detection.Top, detection.Right, detection.Bottom)
+
+        # crop the image
+        #jetson.utils.cudaCrop(img, imgCropped, crop_roi)
+
+        # save the image
+        #jetson.utils.saveImage('object_{:d}.jpg'.format(object_count), imgCropped)
+        #object_count += 1
+        #del imgCropped
+
+    display.Render(img)
+    print("1")
+    display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
