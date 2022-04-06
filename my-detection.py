dedec8304cb63daba992fbb23bf5cfd6e377accb
wlgnsthd <wlgnsthd@mail.ulsan.ac.kr>
2022-04-06 13:34:29 +0900

    commit

--- my-detection.py
+++ my-detection.py
@@ -10,26 +10,10 @@ object_count = 0
 
 while display.IsStreaming():
     img = camera.Capture()
-    detections = net.Detect(img)#overlay='none'
+    detections = net.Detect(img) # ,overlay='none' : no detection image
 
     for detection in detections:
-        # allocate the output image, with the cropped size
-        imgCropped = jetson.utils.cudaAllocMapped(width=detection.Width,
-                                                  height=detection.Height,
-                                                  format=img.format)
-
-        # get the cropping ROI from the bounding box
-        #crop_roi = (detection.Left, detection.Top, detection.Right, detection.Bottom)
         print(detection.Left, detection.Top, detection.Right, detection.Bottom)
 
-        # crop the image
-        #jetson.utils.cudaCrop(img, imgCropped, crop_roi)
-
-        # save the image
-        #jetson.utils.saveImage('object_{:d}.jpg'.format(object_count), imgCropped)
-        #object_count += 1
-        #del imgCropped
-
     display.Render(img)
-    print("1")
     display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
